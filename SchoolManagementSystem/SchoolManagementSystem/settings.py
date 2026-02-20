e9ebdb681ecd6fbcd8e7da97714d699ab486ea54
Nimisha <nimishapc2026@gmail.com>
2026-02-20 08:04:32 +0530

    Updated

--- SchoolManagementSystem/SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/SchoolManagementSystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3)b9zi0#t6&l^2l20!=+kjxxp8xw$r7=fzu9d$o7l^(fwy9j_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nimisha.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
